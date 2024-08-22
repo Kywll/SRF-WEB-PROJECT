f8f0093bf4750c5be052ca7fecab59d3d53fbd63
Kywll <89644541+Kywll@users.noreply.github.com>
2024-08-22 15:33:01 +0800

    Update index.php

--- SRF WEBSITE/index.php
+++ SRF WEBSITE/index.php
@@ -6,6 +6,60 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+        <h2>Welcome to Sacrificials!</h2>
+        username:<br>
+        <input type="text" name="username"><br>
+        password:<br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="submit" value="register">
+        <br>
+    </form>
+</body>
+</html>
+
+
+<?php
+    include("database.php");
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+    
+        if(empty($username)){
+            echo"Please enter a username";
+        }
+        elseif(empty($password)){
+            echo"Please enter a password";
+        }
+        else{
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (user, password)
+                    VALUES ('$username', '$hash')";
+            try{
+            mysqli_query($conn, $sql);
+            echo"You are now registered!";
+            }
+            catch(mysqli_sql_exception){
+                echo"Username is already taken";
+            }
+        }
+    }
+
+    mysqli_close($conn);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     <form action="index.php" method="get">
         <label>IGN:</label>
         <input type="text" name="ign"><br>
@@ -24,12 +78,49 @@
 
 
 <?php
-    include("database.php");
+    
+    /* 
+    $username = "jci";
+    $password = "jancedron";
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+    
+    $sql = "INSERT INTO users (user, password )
+            VALUES ('$username', '$hash')";
+
+    try{
+        mysqli_query($conn, $sql);
+        echo"User is now registered";
+    }
+    catch(mysqli_sql_exception){
+        echo"Could not register user";
+    }
+    
+
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($result) >  0){
+        while ($row = mysqli_fetch_assoc($result)){
+            echo $row["id"] . "<br>";
+            echo $row["user"] . "<br>";
+            echo $row["reg_date"] . "<br>";
+        };
+    }
+    else{
+        echo "No user found";
+    }
+
+    mysqli_close($conn);
+    */
+
+    
+
 
     $ign = $_GET["ign"];
     $role = $_GET["role"];
     $rating = $_GET["rating"];
     $second_role = $_GET["second_role"];
+
 
 
     echo "IGN: {$ign}<br>";
@@ -57,9 +148,6 @@
     /*  Still
         No
         Comment*/
-        
-    
-
 ?>
 
 
