d63693e9bfab4ddd718879ebc51aa196fcf85240
Kywll <89644541+Kywll@users.noreply.github.com>
2024-08-22 08:04:28 +0800

    Add files via upload

---
+++ SRF WEBSITE/database.php
@@ -0,0 +1,25 @@
+<?php
+
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "sacrificialsdb";
+    $conn = "";
+
+    try{
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    }
+    catch(mysqli_sql_exception){
+        echo"Cannot Connect!<br>";
+    }
+
+
+    
+
+
+
+    if($conn){
+        echo"You are connected!<br>";
+    }
+
+?>
