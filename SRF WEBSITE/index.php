b330df0fd80e2de92412c1128459b32aa9897db4
Kywll <89644541+Kywll@users.noreply.github.com>
2024-09-04 21:13:02 +0800

    Update index.php

--- SRF WEBSITE/index.php
+++ SRF WEBSITE/index.php
@@ -1,28 +1,43 @@
+<?php
+    $title = 'Home';
+    $page = 'home';
+    include_once('navbar.php')
+?>  
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-    
+    <!--
     <img src="images/aksrf.jpeg" width="325px" alt="125px">
     <h1>MAIN PAGE</h1>
     <br><br><br><br><br><br><br><br><br><br>
     
+
+    <button>HOME<br></button>
+    <button>ABOUT<br></button>
+    <button>MEMBERS<br></button>
     <form action="tryout.php">
         <button>Join Us<br></button>
     </form>
     <form action="login.php">
-        <button>LOGOUT<br></button>
+        <button>LOGIN<br></button>
     </form>
+    <button>LOGOUT<br></button>
+    -->
+
+
 </body>
 </html>
     
     
 <?php
     session_start();
+    include("database.php");
     
 ?>
