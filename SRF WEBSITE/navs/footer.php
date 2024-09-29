060cf2fb91a6173dd340cd7a00a90e51b866bfb7
Kywll <89644541+Kywll@users.noreply.github.com>
2024-09-29 10:16:19 +0800

    Update footer.php

--- SRF WEBSITE/navs/footer.php
+++ SRF WEBSITE/navs/footer.php
@@ -1,1 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+<hr>
+    <footer>
+    
+    <div class="vetical-menu" align="center" id="main-texts">
+    
+            <div id="footer-div">
+            <div id="hyztt">
+            <img src="images/hyztt.jpg" alt="">
+            <h3 id="comp-name">Hyztt</h3>
+            <p>Creator of the Sacrificials Website</p>
+            </div>
+            </div>
 
+            <div id="footer-div">
+            <h3>Browse</h3>
+
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="members.php">Members</a></li>
+                <li><a href="tryout.php">Join Us</a></li>
+                <li><a href="login.php">Login</a></li>
+            </ul>
+            </div>
+
+            <div id="footer-div">
+            <h3>Info</h3>
+                <p>Thank you for visiting my website! I’m looking forward to take on new challenges and collaborate on exciting projects. If you’re interested in working together, feel free to reach out.</p>
+            </div>
+
+
+            <div id="footer-div">
+            <div id ="contact">
+            <h3>Contact</h3>
+            <ul>
+                <li><img src="images/icons/location-icon.png" alt="">Muntinlupa,Philippines</li>
+                <li><img src="images/icons/mail-icon.png" alt="">selopanmichael@gmail.com</li>
+                <li><img src="images/icons/telephone-icon.png" alt="">09152636891</li>
+                <li><img src="images/icons/facebook-icon.png" alt=""><a href="https://www.facebook.com/michael.selopan/">Michael Napoles</a></li>
+                <li><img src="images/icons/github-icon.png" alt=""><a href="https://github.com/Kywll">Github</a></li>
+                <li><img src="images/icons/linkedin-icon.png" alt=""><a href="https://www.linkedin.com/in/michael-napoles-613b2a32b/">LinkedIN</a></li>
+            </ul>
+            </div>
+            </div>
+    </div>
+
+    <div id="last-texts">
+    <p id="copyright">Copyright 2024</p>
+    <p id="privacy-policy">Privacy Policy</p>
+    </div>
+
+    </footer>
+
+    
+
+</body>
+</html>
