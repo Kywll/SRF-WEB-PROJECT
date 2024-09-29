5870a04388d9112180ad5ad0e7bf9e1f6e577b79
Kywll <89644541+Kywll@users.noreply.github.com>
2024-09-29 10:10:27 +0800

    Update members.php

--- SRF WEBSITE/members.php
+++ SRF WEBSITE/members.php
@@ -1,7 +1,7 @@
 <?php
     $title = 'Members';
     $page = 'members';
-    include_once('navbar.php')
+    include_once('navs/navbar.php')
 ?>  
 
 <!DOCTYPE html>
@@ -10,14 +10,89 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/members.css">
 </head>
 <body>
     
+    <img src="images/bots.png" alt="" id="bots-pic">
+    <h1 id="title"><strong>MEMBERS</strong></h1>
+
+    <div id="all-members">
+        <h1 id="main-title">SRF Members:</h1>
+        <br>
+
+        <div id="members">
+            <img src="images/JC_PROFILE.jpg" alt="" id="profile-jci">
+            <div id="content">
+                <h2>Jci</h2>
+                <p id="jci-text">Rifler of SRF</p>
+                <div id="card-left">
+                </div>
+            </div>
+        </div>
+
+        <div id="members">
+            <img src="images/jasteen.jpg" alt="" id="profile-jasteen">
+            <div id="content-middle">
+                <h2>Jasteen</h2>
+                <p>Awper of SRF</p>
+            </div>
+            <div id="card-middle">
+            </div>
+        </div>
+
+        <div id="members">
+            <img src="images/hyztt.jpg" alt="" id="profile-hyztt">
+            <div id="content-right">
+                <h2>Hyztt</h2>
+                <p>IGL of SRF</p>
+                <di id="card-right">
+                </div>
+            </div>
+        </div>
+
+        <br><br><br>
+
+        <div id="bottom">
+        <div id="members">
+            <img src="images/solstice.jfif" alt="" id="profile-solstice">
+            <div id="content-left">
+                <h2>Solstice</h2>
+                <p>Support of SRF</p>
+                <div id="card-left">
+                </div>
+            </div>
+        </div>
+
+        <div id="members">
+            <img src="images/cj.webp" alt="" id="profile-mark">
+            <div id="mark">
+                <h2>Mark</h2>
+                <p>Entry Fragger of SRF</p>
+            </div>
+            <div id="card-middle">
+            </div>
+        </div>
+
+        <div id="members">
+            <img src="images/thad.png" alt="" id="profile-thad">
+            <div id="thad">
+                <h2>Thad</h2>
+                <p>Lurker of SRF</p>
+                <div id="card-right">
+                </div>
+            </div>
+        </div>
+        </div>
+
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
 </body>
 </html>
 
 
 
 <?php
-
+    include_once('navs/footer.php');
 ?>
